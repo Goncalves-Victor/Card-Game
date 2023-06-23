48e272e3385a093fc6a0de6e8401dba1572ee749
Joao Rocha <joaovitorfvr@gmail.com>
2023-06-23 17:08:00 -0300

    Adiciona movimento ao clicar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var cartas = [
+/*var cartas = [
     { forca: 1, energia: 1 },
     { forca: 2, energia: 2 },
     { forca: 3, energia: 3 },
@@ -37,6 +37,9 @@ function criaCarta(){
 
         carta.addEventListener('click', function() {
             carta.classList.toggle('scaled');
+
+            const lugarUm = document.getElementById('ump');
+            lugarUm.appendChild(carta); // Adiciona a carta à div "lugar um"
         });
     }
 }
@@ -45,4 +48,91 @@ const botaoInicio = document.getElementById('iniciar');
 botaoInicio.addEventListener('click', criaCarta);
 
 const botaoreset = document.getElementById('reset');    
+botaoreset.addEventListener('click', reset);*/
+
+var cartas = [
+    { forca: 1, energia: 1 },
+    { forca: 2, energia: 2 },
+    { forca: 3, energia: 3 },
+    { forca: 4, energia: 4 },
+    { forca: 5, energia: 5 },
+    { forca: 6, energia: 6},
+    { forca: 7, energia: 7 },
+    { forca: 8, energia: 8 },
+    { forca: 9, energia: 9 },
+    { forca: 10, energia: 10 },
+    { forca: 11, energia: 11 },
+    { forca: 12, energia: 12 }
+];
+
+var selectedCard = null;
+
+function reset(){
+    location.reload();
+}
+
+function criaCarta(){
+    const deck = document.querySelector('.deck');
+    for(let i=0; i < 4; i++){
+        const indice = i;
+        const carta = document.createElement('div');
+        carta.classList.add('card');
+        carr = deck.appendChild(carta);
+
+        const forca = document.createElement('span');
+        forca.classList.add('forca');
+        forca.innerHTML = cartas[indice].forca;
+        carr.appendChild(forca);
+        
+        const energia = document.createElement('span');
+        energia.classList.add('energia');
+        energia.innerHTML = cartas[indice].energia;
+        carr.appendChild(energia);
+
+        carta.addEventListener('click', function() {
+            carta.classList.toggle('scaled');
+            selectedCard = carta;
+        });
+    }
+}
+
+function moveCardToUmp() {
+    if (selectedCard !== null) {
+        const lugarUm = document.getElementById('ump');
+        lugarUm.appendChild(selectedCard);
+        selectedCard = null;
+    }
+}
+
+function moveCardToDoisp() {
+    if (selectedCard !== null) {
+        const lugarDois = document.getElementById('doisp');
+        lugarDois.appendChild(selectedCard);
+        selectedCard = null;
+    }
+}
+
+function moveCardToTresp() {
+    if (selectedCard !== null && localTresp) {
+        const lugarTres = document.getElementById('tresp');
+        lugarTres.appendChild(selectedCard);
+        selectedCard = null;
+    }
+}
+
+const botaoInicio = document.getElementById('iniciar');    
+botaoInicio.addEventListener('click', criaCarta);
+
+const botaoreset = document.getElementById('reset');    
 botaoreset.addEventListener('click', reset);
+
+
+
+const localUmp = document.getElementById('um');
+localUmp.addEventListener('click', moveCardToUmp);
+
+const localDoisp = document.getElementById('dois');
+localDoisp.addEventListener('click', moveCardToDoisp);
+
+const localTresp = document.getElementById('tres');
+localTresp.addEventListener('click', moveCardToTresp);
