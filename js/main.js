7cb449b6d105b8d3b20db166e892162b581c6059
Joao Rocha <joaovitorfvr@gmail.com>
2023-06-23 16:18:06 -0300

    Adiciona vetor de cartas contendo forca e energia

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var cartas = [
+    { forca: 1, energia: 1 },
+    { forca: 2, energia: 2 },
+    { forca: 3, energia: 3 },
+    { forca: 4, energia: 4 },
+    { forca: 5, energia: 5 },
+    { forca: 6, energia: 6},
+    { forca: 7, energia: 7 },
+    { forca: 8, energia: 8 },
+    { forca: 9, energia: 9 },
+    { forca: 10, energia: 10 },
+    { forca: 11, energia: 11 },
+    { forca: 12, energia: 12 }
+];
